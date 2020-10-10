b407860744b37ce888aad695812217c21f1cefd6
Danmar Varela <varela.danmar@ue.edu.ph>
2020-10-11 04:25:29 +0800

    modified header logo

--- misc/header.php
+++ misc/header.php
@@ -2,7 +2,8 @@
 <header>
     <div class="header-contents-container">
         <div class="logo">
-            <h1>Meow! The Coding Cat</h1>
+            <img src="../src/redBrownCat.svg" alt="Failed to load image">
+            <h1>The Coding Cat</h1>
         </div>
     </div>
 </header>
