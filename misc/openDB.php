d523086ef866c90efba00a123eb8bbd51f2c8237
Michael Joshua P. Frayna <frayna.michaeljoshua@ue.edu.ph>
2020-10-11 09:47:14 +0800

    Edited Database

--- misc/openDB.php
+++ misc/openDB.php
@@ -4,12 +4,13 @@ $db_username = "root";
 $db_password = "";
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=loginSignup", $db_username, $db_password);
+    $conn = new PDO("mysql:host=$servername;dbname=meowcodingcatdb", $db_username, $db_password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
   } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
   }
+  
 
 ?>
